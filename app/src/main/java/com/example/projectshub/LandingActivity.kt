92326d89433da16c65cd6a19f035d9762000a210
Pratyush Singh <aries.pratyush@gmail.com>
2022-11-01 18:58:35 +0530

    Added login and signup page

--- app/src/main/java/com/example/projectshub/LandingActivity.kt
+++ app/src/main/java/com/example/projectshub/LandingActivity.kt
@@ -1,12 +1,24 @@
 package com.example.projectshub
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
 
 class LandingActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing)
         supportActionBar?.hide()
+        val btnLogin=findViewById<MaterialButton>(R.id.login)
+        btnLogin.setOnClickListener{
+            val intent=Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
+        val btnRegister=findViewById<MaterialButton>(R.id.register)
+        btnRegister.setOnClickListener{
+            val intent=Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/projectshub/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.projectshub
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignUpActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+        supportActionBar?.hide()
+    }
+}
