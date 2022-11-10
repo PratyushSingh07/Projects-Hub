524b22c0ed2160673f90da8844121a3f954d81aa
Pratyush Singh <aries.pratyush@gmail.com>
2022-11-10 21:53:48 +0530

    Added the fragments

---
+++ app/src/main/java/com/example/projectshub/DashboardActivity.kt
@@ -0,0 +1,36 @@
+package com.example.projectshub
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class DashboardActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dash)
+        supportActionBar?.hide()
+        val btn =findViewById<BottomNavigationView>(R.id.bottomNavView)
+        btn.background=null
+        btn.menu.getItem(2).isEnabled=false
+
+        val homeFragment=HomeFragment()
+        val searchFragment=SearchFragment()
+        val bookmarkFragment=BookmarkFragment()
+        val settingsFragment=SettingsFragment()
+
+        btn.setOnNavigationItemReselectedListener{
+            when(it.itemId){
+                R.id.home->setCurrentFragment(homeFragment)
+                R.id.searchBar->setCurrentFragment(searchFragment)
+                R.id.Bookmark->setCurrentFragment(bookmarkFragment)
+                R.id.settings->setCurrentFragment(settingsFragment)
+            }
+        }
+    }
+    private fun setCurrentFragment(fragment: Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,fragment)
+            commit()
+        }
+}
--- app/src/main/java/com/example/projectshub/LoginActivity.kt
+++ app/src/main/java/com/example/projectshub/LoginActivity.kt
@@ -1,12 +1,19 @@
 package com.example.projectshub
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
 
 class LoginActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         supportActionBar?.hide()
+        val btn=findViewById<MaterialButton>(R.id.loginBtnMain)
+        btn.setOnClickListener{
+            val intent=Intent(this,DashboardActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
