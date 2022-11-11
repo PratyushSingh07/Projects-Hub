6fb42ed22bc89bafe79cf42320299b5bbb9ca4b8
Pratyush Singh <aries.pratyush@gmail.com>
2022-11-11 15:42:52 +0530

    Fragment and Animation

---
+++ app/src/main/java/com/example/projectshub/AddProjectFragment.kt
@@ -0,0 +1,24 @@
+package com.example.projectshub
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.button.MaterialButton
+
+class AddProjectFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_project, container, false)
+    }
+}
--- app/src/main/java/com/example/projectshub/DashboardActivity.kt
+++ app/src/main/java/com/example/projectshub/DashboardActivity.kt
@@ -1,9 +1,12 @@
 package com.example.projectshub
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class DashboardActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,9 +29,25 @@ class DashboardActivity:AppCompatActivity() {
                 R.id.home->setCurrentFragment(homeFragment)
                 R.id.searchBar->setCurrentFragment(searchFragment)
                 R.id.Bookmark->setCurrentFragment(bookmarkFragment)
-                R.id.settings->setCurrentFragment(settingsFragment)
+                R.id.settings-> {
+                    supportFragmentManager.beginTransaction().apply {
+                        setCustomAnimations(R.anim.slide_to_top,R.anim.slide_to_top)
+                        replace(R.id.flFragment,settingsFragment)
+                        commit()
+                    }
+                }
             }
-            true// ye likhne se icon change hote hue dikega but false likhne se frag change hoga but icon nhi
+            true // ye likhne se icon change hote hue dikega but false likhne se frag change hoga but icon nhi
+        }
+        val addProj=findViewById<FloatingActionButton>(R.id.fab)
+        addProj.setOnClickListener{
+            findViewById<BottomAppBar>(R.id.bottomAppBar).visibility= View.GONE
+            addProj.visibility=View.GONE
+            supportFragmentManager.beginTransaction().apply {
+                setCustomAnimations(R.anim.slide_to_top,R.anim.slide_to_top)
+                replace(R.id.flFragment,AddProjectFragment())
+                commit()
+            }
         }
     }
     private fun setCurrentFragment(fragment: Fragment)=
